4b2d8ec932b6c205f5cb41712dcf6249ebd9303b
presespdes1 <presespdes1@gmail.com>
2024-05-26 21:42:17 -0400

    Instalando JWT Auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(){
+
+    }
+
+    public function register()
+    {
+
+    }
+
+    public function logout()
+    {
+
+    }
+
+    public function refresh()
+    {
+        
+    }
+}
